07537a30fb5bbb40e69dbac3b48fcd2c4a5ecc69
Admira1B <Vreizenbuk@mail.ru>
2022-03-28 01:50:27 +0700

    first fix

--- Program.cs
+++ Program.cs
@@ -13,15 +13,19 @@ namespace RPM
             int repeatCount = 0;
             System.Console.WriteLine("Кол-во файлов для создания: ");
             int numOfReprat = int.Parse(Console.ReadLine());
+            
             do
             {
                 string folderPATH = @"";
                 folderPATH = System.IO.Path.Combine(folderPATH, folder);
                 string fileName = System.IO.Path.GetRandomFileName();
+                folderPATH = System.IO.Path.Combine(folderPATH, fileName);
                 System.IO.File.Create(folderPATH);
 
-                numOfReprat++;
-            } while (repeatCount == numOfReprat );
+                repeatCount++;
+                System.Console.WriteLine("Файл создан " + repeatCount);
+            } while (repeatCount != numOfReprat);
+            
             System.Console.WriteLine("Нажмите любую клавишу для продолжения.");
             Console.ReadKey();
         }
