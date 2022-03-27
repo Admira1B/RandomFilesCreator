f216757c2b665fa470b55641871841cb32547965
Admira1B <Vreizenbuk@mail.ru>
2022-03-28 00:18:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace RPM
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите путь к папке, где вы хотите создать файл(ы):");
+            string? folder = Console.ReadLine();
+            int repeatCount = 0;
+            System.Console.WriteLine("Кол-во файлов для создания: ");
+            int numOfReprat = int.Parse(Console.ReadLine());
+            do
+            {
+                string folderPATH = @"";
+                folderPATH = System.IO.Path.Combine(folderPATH, folder);
+                string fileName = System.IO.Path.GetRandomFileName();
+                System.IO.File.Create(folderPATH);
+
+                numOfReprat++;
+            } while (repeatCount == numOfReprat );
+            System.Console.WriteLine("Нажмите любую клавишу для продолжения.");
+            Console.ReadKey();
+        }
+    }
+}
